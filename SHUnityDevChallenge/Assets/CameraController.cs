a3304c54f3f8f9336092dfe005edc36f8e1cf3ca
Neosh Sheikh <neosh11@gmail.com>
2021-03-03 23:50:30 +0800

    Working dark mode
    + Time spent updating color scheme

---
+++ SHUnityDevChallenge/Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraController : MonoBehaviour
+{
+    static readonly Color DARK = new Color(0.247f, 0.275f, 0.282f);
+    static readonly Color LIGHT = new Color(0.569f, 0.635f, 0.725f);
+
+    private Camera cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        // Set light theme by default
+        cam.backgroundColor = LIGHT;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DarkMode(bool on)
+    {
+        if (on) cam.backgroundColor = DARK;
+        else cam.backgroundColor = LIGHT;
+    }
+}
--- SHUnityDevChallenge/Assets/DarkModeToggle.cs
+++ SHUnityDevChallenge/Assets/DarkModeToggle.cs
@@ -5,16 +5,19 @@ using TMPro;
 
 public class DarkModeToggle : MonoBehaviour
 {
+
+    [SerializeField] CameraController cameraController;
     // Because of the set up,
     // 0 = off
     // 1 = on
     public void OnValChange(float val)
     {
+
         int on = (int)val;
-        // switch (on)
-        // {
-        //     case 1: darkModeText.text = "ON"; break;
-        //     default: darkModeText.text = "OFF"; break;
-        // }
+        switch (on)
+        {
+            case 1: cameraController.DarkMode(true); break;
+            default: cameraController.DarkMode(false); break;
+        }
     }
 }
