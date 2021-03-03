1ea5960fb1f77da90a4e4d5b0aa800aecdb9cb54
Neosh Sheikh <neosh11@gmail.com>
2021-03-03 21:41:29 +0800

    Code refactor in an attempt to reuse classes/enums

---
+++ SHUnityDevChallenge/Assets/CalcUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+
+public enum InputDataType
+{
+    NUMERIC,
+    OPERATION,
+    SUBMENU
+}
+
+public enum InputOperation
+{
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    POW,
+    SQRT
+}
+
+
+// Defines what an input is on a calculator
+public class CalcInputs
+{
+    // If the type is an operation or a number
+    public InputDataType type;
+    // in the case of numeric - text = value
+    public string text;
+    // Optional only if type is operation
+    public InputOperation operation;
+    // Create a submenu from these
+    public List<CalcInputs> subOptions;
+
+}
--- SHUnityDevChallenge/Assets/DropdownController.cs
+++ SHUnityDevChallenge/Assets/DropdownController.cs
@@ -4,41 +4,6 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public enum InputDataType
-{
-    NUMERIC,
-    OPERATION,
-    SUBMENU
-}
-
-public enum InputOperation
-{
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    POW,
-    SQRT
-}
-
-
-// Defines what an input is on a calculator
-public class CalcInputs
-{
-    // If the type is an operation or a number
-    public InputDataType type;
-    // in the case of numeric - text = value
-    public string text;
-    // Optional only if type is operation
-    public InputOperation operation;
-    // Create a submenu from these
-    public List<CalcInputs> subOptions;
-
-}
-
-
-
-
 public class DropdownController : MonoBehaviour
 {
     // Currently set up in a dodgy manner
