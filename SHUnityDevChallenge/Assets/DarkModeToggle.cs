d455ddf04ff99d918551d396eaeb6fa9bed5d067
Neosh Sheikh <neosh11@gmail.com>
2021-03-03 23:31:43 +0800

    Built "Dark Mode" toggle that does nothing

---
+++ SHUnityDevChallenge/Assets/DarkModeToggle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DarkModeToggle : MonoBehaviour
+{
+    // Because of the set up,
+    // 0 = off
+    // 1 = on
+    public void OnValChange(float val)
+    {
+        int on = (int)val;
+        // switch (on)
+        // {
+        //     case 1: darkModeText.text = "ON"; break;
+        //     default: darkModeText.text = "OFF"; break;
+        // }
+    }
+}
